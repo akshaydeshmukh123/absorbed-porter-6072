c2b04703562815dcff3c53881a7f8d7452e1e50f
akshaydeshmukh123 <akshaydeshmukh9996@gmail.com>
2023-01-19 11:43:50 +0530

    day-02

---
+++ baby-cry/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Extranavbar from './Componants/Navbar/Extranavbar';
+import LowerNavbar from './Componants/Navbar/LowerNavbar';
+
+function App() {
+  return (
+    <div className="App">
+  <Extranavbar/>
+    <LowerNavbar/>
+    </div>
+  );
+}
+
+export default App;
